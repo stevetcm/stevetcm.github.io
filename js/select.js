069cfb7287bcd5d0f6546f34d6b6ffa134d2d873
Steve TCM <stseachinming@uwaterloo.ca>
2016-03-04 17:17:11 -0500

    Added Mobile-Friendly Navigation Bar

---
+++ js/select.js
@@ -0,0 +1,43 @@
+window.onload = function() { 
+  var btn1 = document.getElementById("menu1");
+  var btn2 = document.getElementById("menu2");
+	var btn3 = document.getElementById("menu3");
+	var btn4 = document.getElementById("menu4");
+
+	btn1.onclick = function() {
+		console.log(getComputedStyle(menu2).display);
+		if (getComputedStyle(menu2).display == "block") {
+			menu2.style.display = "none";
+			menu3.style.display = "none";
+	  	menu4.style.display = "none";
+		} else {
+			menu2.style.display = "block";
+	  	menu3.style.display = "block";
+	  	menu4.style.display = "block";
+	  }
+	}
+
+	btn2.onclick = function() {
+		if (getComputedStyle(menu1).display == "block") {
+			menu2.style.display = "none";
+			menu3.style.display = "none";
+	  	menu4.style.display = "none";
+		}
+	}
+
+	btn3.onclick = function() {
+		if (getComputedStyle(menu1).display == "block") {
+			menu2.style.display = "none";
+			menu3.style.display = "none";
+	  	menu4.style.display = "none";
+		}
+	}
+
+	btn4.onclick = function() {
+		if (getComputedStyle(menu1).display == "block") {
+			menu2.style.display = "none";
+			menu3.style.display = "none";
+	  	menu4.style.display = "none";
+		}
+	}
+};
